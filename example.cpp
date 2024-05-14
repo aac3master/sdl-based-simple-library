9a11470cf29357aba9868e5387d50822206415e5
aac3master <96526713+aac3master@users.noreply.github.com>
2024-05-14 15:53:48 +0300

    Added new thing! (UpdateAll() now useless)
    Really sorry for memory problem

--- example.cpp
+++ example.cpp
@@ -15,12 +15,15 @@ int main( int argc, char * argv[] ) //For user: add here quit bool to control wh
     Sprite my_sprite;//creating new instance of sprite
     Sprite my_sprite2;//creating new instance of sprite
     my_sprite.texture_path = "smile.png";//adding texture (use .UpdateAll() to commit changes!!!)
+    //heh, i got new error! so now to commit texture you need to use SetTexture() (DONT PUT THIS INTO WHILE OR YOU WILL GET A BAD TIMES!!!)
+    my_sprite.SetTexture();
     my_sprite.x = 500;//set x pos (also can be y for hight), in example making sprite be almost in center
     my_sprite.sx = 100;//sets x size (sx = size x, sy = size y) in pixels (Warning! Sprite will render 100x100 by default! If you have sprite (image) with another size, please set sx and sy like you image size!!!)
     my_sprite.angel = 50;// angel
 
 
     my_sprite2.texture_path = "smile.png";//
+    my_sprite2.SetTexture();
     my_sprite2.x = 200;//
     my_sprite2.y = 600;
     my_sprite2.sx = 300;//
@@ -34,7 +37,7 @@ int main( int argc, char * argv[] ) //For user: add here quit bool to control wh
     my_sprite2.UpdateAll();//
     while (true)//Simple game loop! (while is part of c++)
     {
-        //my_sprite.sy++;//funny o:
+        //my_sprite.sx++;//funny o:
         my_sprite.UpdateAll();//Add it in loop only if you changing sprite! (after commands of change, like: my_sprite.texture_path = "smile.png";
         my_sprite2.UpdateAll();//
         UpdateTick(my_window);
